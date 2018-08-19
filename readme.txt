079b18943023d0536f20aea19ba51f7f294cff4a
Dominik Rockenschaub SICONET <dominik.rockenschaub@siconet.at>
2018-08-19 10:10:06 +0200

    Added readme.txt and first version of plugin

---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Google Analytics Opt-Out Link Shortcode by SICONET ===
+Contributors: siconet
+Tags: google, analytics, gdpr, link
+Requires at least: 4.9
+Tested up to: 4.9.4
+Requires PHP: 5
+License: GNU General Public License v3.0
+License URI: https://choosealicense.com/licenses/gpl-3.0/
+
+Provide a simple shortcode to display a customizeable Google Analytics Cookie Opt-Out link in Wordpress required according to the GDPR.
+
+== Description ==
+Provide a simple shortcode to display a customizeable Google Analytics Cookie Opt-Out link in Wordpress required according to the GDPR.
+
+Usage
+[siconet_google_optout_cookie_link title=\"Für Google Analytics Opt-Out: Bitte klicken!\" gaid=\"UA-XXXXX\" cssclass=\"your-custom-classname\" showalertafterclick=\"true\" alertmsg=\"Google Cookies werden nicht mehr gespeichert.\"] 
+
+== Installation ==
+1) Download plugin
+2) Install and activate Plugin
+3) Use shortcode like in the description
+
+== Frequently Asked Questions ==
+
+
+== Changelog ==
+First version of this plugin, provide a simple shortcode
---
+++ siconet-google-analytics-opt-out-link-shortcode.php
@@ -0,0 +1,59 @@
+<?php
+/*
+Plugin Name:  Google Analytics Opt-Out Link Shortcode by SICONET
+Plugin URI:   https://developer.wordpress.org/plugins/the-basics/
+Description:  Provide a simple shortcode to display a customizeable Google Analytics Cookie Opt-Out link in Wordpress required according to the GDPR.
+Version:      20180819
+Author:       SICONET Dominik Rockenschaub
+Author URI:   https://www.siconet.at
+License:      GNU General Public License v3.0
+License URI:  https://choosealicense.com/licenses/gpl-3.0/
+Text Domain:  siconet-google-analytics-opt-out-link
+Domain Path:  /languages
+*/
+
+defined( 'ABSPATH' ) or die( 'Nice try - no script kiddies please!' );
+
+/*---------------------------------------------------------------------------------------------------------------------------*/
+/**
+ * i18n
+ */
+/*--------------------------------------------------------------------------------------------------------------------------*/
+load_plugin_textdomain('siconet-google-analytics-opt-out-link', false, basename( dirname( __FILE__ ) ) . '/languages' );
+
+/*---------------------------------------------------------------------------------------------------------------------------*/
+/**
+ * Shortcode
+ * Render a custom Google Analytics Opt Out Cookie Link
+ */
+/*--------------------------------------------------------------------------------------------------------------------------*/
+function siconet_shortcode_google_optout_cookie_link($atts)
+{
+    extract(shortcode_atts(array(
+        'title' => __('Für Google Analytics Opt-Out: Bitte klicken!', 'siconet-google-analytics-opt-out-link'),
+        'alertmsg' => __('Google Cookies werden nicht mehr gespeichert.', 'siconet-google-analytics-opt-out-link'),
+        'showalertafterclick' => true,
+        'gaid' => 'UA-XXX',
+        'cssclass' => 'siconet-google-analytics-opt-out-link'
+    ), $atts));
+    ob_start();
+    ?>  
+    <!-- Google Opt-Out Function -->
+    <script>
+      var gaProperty = '<?php echo $gaid; ?>';
+      
+      var disableStr = 'ga-disable-' + gaProperty; if (document.cookie.indexOf(disableStr + '=true') > -1) { window[disableStr] = true; }
+      
+        function gaOptout() { 
+            document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/'; 
+            window[disableStr] = true; 
+            <?php if ($showalertafterclick == "true" || $showalertafterclick === true) : ?>
+                alert(<?php echo $alertmsg; ?>);
+            <?php endif; ?>
+        }
+    </script>
+    <a href="javascript:gaOptout()" class="<?php echo $cssclass; ?>"><?php echo $title; ?></a>
+    <?php
+    return ob_get_clean();
+}
+add_shortcode('siconet_google_optout_cookie_link', 'siconet_shortcode_google_optout_cookie_link');
